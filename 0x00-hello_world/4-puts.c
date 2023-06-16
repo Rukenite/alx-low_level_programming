4430e3f2f4055a81b95667351df21463b473b43a
rukenite <rukenite67@gmail.com>
2023-06-15 19:24:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void){
+/**
+ * main- A program to print a sentece with puts function
+ *  Return: (0)(Success)
+ */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
-return (0)
+return (0);
 }
