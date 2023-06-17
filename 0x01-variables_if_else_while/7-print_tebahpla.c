4a0947d4dd98567ebda08ba2ea86e1e6a9227dc0
rukenite <rukenite67@gmail.com>
2023-06-16 20:01:07 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry execution
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l >= 'a'; l--)
+	putchar(l);
+	putchar('\n');
+
+	return (0);
+
+}
+	
