3ae984d1a1bdc9ab8673bcd2a5b8ef5c09945c9a
rukenite <rukenite67@gmail.com>
2023-06-16 19:20:45 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main- Entry point
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	char l, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+	if (l != e && l != q)
+	putchar(l);
+	}
+	putchar('\n');
+	return (0);
+
+}
