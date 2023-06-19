8e2a6431793be6a4eea0a616edc64c40c6cad34d
rukenite <rukenite67@gmail.com>
2023-06-19 09:24:45 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * A program that prints a statement to the standard error
- * Return: 1 (successful)
+ * main - Prints to the standard error
+ * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\n",stdout)
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
